d2fda0b0997ce9a17ae95671f2515f89eded5bed
PrakashKoneru <PrakashKoneru@Sivas-MBP.T-mobile.com>
2017-03-25 15:01:52 -0400

    Added a few tests that check for the existance of some HTML tags and a few chai matches

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,12 +2,14 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
+	let component;
+
+	beforeEach(() => {
+		component = renderComponent(App);
+	})
     
-    it('renders something', () => {
-	    
-	    const component = renderComponent(App);
-    	
-    	expect(component).to.contain('React simple starter');
-  });
+    it('shows a comment box', () => {
+		expect(component.find('.comment-box')).to.exist;    
+  	});
 });
 

2cc1b83cefbeb175eff0a7f5846a53ac342515f4
PrakashKoneru <PrakashKoneru@Sivas-MBP.T-mobile.com>
2017-03-25 13:44:31 -0400

    Initialized a sample test to check if my component has the required string

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,13 +2,12 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
-  let component;
-
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+    
+    it('renders something', () => {
+	    
+	    const component = renderComponent(App);
+    	
+    	expect(component).to.contain('React simple starter');
   });
 });
+
